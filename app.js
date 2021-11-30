b9ebeb707a646f929ba5e7b2a652c3a46c6923ab
thedon122 <donovan122@yahoo.com>
2021-11-30 02:02:00 -0500

    rename app3

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-const readline = require('readline');
-const rl = readline.createInterface({input : process.stdin,
-                        output : process.stdout});
-let num1 = Math.floor((Math.random() * 10) + 1);
-let num2 = Math.floor((Math.random() * 10) + 1);
-let answer = num1 + num2;
-
-rl.question(`What is ${ num1 } + ${ num2 }? \n`,
-(userInput)=>{
-    if(userInput.trim() == answer){
-        rl.close();
-    }
-    else{
-        rl.setPrompt('Incorrect response please try again\n');
-        rl.prompt();
-        rl.on('line',(userInput)=>{
-            if(userInput.trim() == answer)
-                rl.close();
-            else{
-                rl.setPrompt(`Your answer of ${ userInput } is incorrect try again \n`)
-                rl.prompt();
-            }
-        })
-    }
-});
-
-rl.on('close',()=>{
-    console.log('Correct!!!!');
+const fs = require('fs');
+// create a file
+fs.writeFile('example.txt',"this ia an example",(err)=>{
+    if(err)
+        console.log(err);
+    else
+        console.log('File successfully created');
 });

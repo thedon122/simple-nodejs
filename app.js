69bf5b434fde5a408afdc76924d0e26778b88d8f
thedon122 <donovan122@yahoo.com>
2021-11-29 23:55:48 -0500

    update

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let num1 = Math.floor((Math.random() * 10) + 1);
 let num2 = Math.floor((Math.random() * 10) + 1);
 let answer = num1 + num2;
 
-rl.question(`What is ${ num1 } + ${ num2 }`,
+rl.question(`What is ${ num1 } + ${ num2 }? \n`,
 (userInput)=>{
-    console.log(userInput);
+    if(userInput.trim() == answer){
+        rl.close();
+    }
 });

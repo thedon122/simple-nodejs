1fc76b1b9b74650818402135cfa320142b082c4f
thedon122 <donovan122@yahoo.com>
2021-11-30 02:07:00 -0500

    first fs app

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const fs = require('fs');
 // create a file
-fs.writeFile('example.txt',"this ia an example",(err)=>{
+// fs.writeFile('example.txt',"this ia an example",(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('File successfully created');
+//         fs.readFile('example.txt',utf8,(err,file)=>{
+//             if(err)
+//                 console.log(err);
+//             else
+//                 console.log(file);
+//         })
+// });
+// fs.rename('example.txt','example2.txt',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('successfully renamed the file');
+// });
+
+// fs.appendFile('example2.txt','Some data being appended',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('Successfully appended data to file');
+// });
+
+fs.unlink('example2.txt',(err)=>{
     if(err)
         console.log(err);
     else
-        console.log('File successfully created');
+        console.log('successfully deleted the file');
 });

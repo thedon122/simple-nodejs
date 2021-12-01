4e62f89603e87621748086ef2f6a2b719b938965
thedon122 <donovan122@yahoo.com>
2021-12-01 13:27:00 +0100

    stream update

---
+++ app5.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+// fs.mkdir('tutorial',(err)=>{
+//     if(err)
+//         console.log(err)
+//     else
+//         fs.writeFile('./tutorial/example.txt','123',(err)=>{
+//             if(err)
+//                 console.log(err);
+//             else{
+//                 console.log('successfully created file');
+//             }
+//         })
+// })
+
+// fs.unlink('./tutorial/example.txt',(err)=>{
+//     if(err)
+//         console.log(err)
+//     else{
+//         fs.rmdir('tutorial',(err)=>{
+//             if(err)
+//                 console.log(err);
+//             else{
+//                 console.log('deleted folder');
+//             }
+//         });
+//     }
+// });
+
+fs.readdir('example',(err,files)=>{
+    if(err)
+        console.log(err);
+    else{
+        for(let file of files){
+            fs.unlink('./example/' + file,(err)=>{
+                if(err)
+                    console.log(err);
+                else{
+                    console.log('successfully deleted file');
+                }
+            })
+        }
+    }
+})

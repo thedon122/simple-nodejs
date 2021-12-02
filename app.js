f33dfd4e0d454cb4cf20855e0cc3a6aba2671464
thedon122 <donovan122@yahoo.com>
2021-12-02 01:00:00 +0100

    packagejson creation

--- app.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/example.png');
-    res.writeHead(200,{'Content-type': 'image/png'});
-    readStream.pipe(res);
-}).listen(3000);

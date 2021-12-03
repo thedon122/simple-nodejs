217b9f8037b0760590977c2d322a903773cc10fa
thedon122 <donovan122@yahoo.com>
2021-12-03 18:09:00 +0100

    json update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'));
@@ -13,7 +14,7 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{
     console.log(req.body);
     // database work here
-    res.send('successfully posted data');
+    res.json({success : true});
 });
 
 app.listen(3000);

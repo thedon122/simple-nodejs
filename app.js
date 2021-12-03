75f9465a8c3cff81a6b0b372b27a1c8be438c4b5
thedon122 <donovan122@yahoo.com>
2021-12-03 16:12:00 +0100

    express update

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
+app.get('/example',(req,res)=>{
+    res.send('hitting example route');
+});
+
+app.get('/example/:name/:age',(req,res)=>{
+    console.log(req.params);
+    console.log(req.query)
+    res.send(req.params.name + " : " + req.params.age);
+});
+
 app.listen(3000);

c051702ef55420e1b27f684cb99097a7935832ba
thedon122 <donovan122@yahoo.com>
2021-12-03 17:39:00 +0100

    body-parser update

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname,'static','index.html'));
 });
 
-app.get('/example',(req,res)=>{
-    res.send('hitting example route');
-});
-
-app.get('/example/:name/:age',(req,res)=>{
-    console.log(req.params);
-    console.log(req.query)
-    res.send(req.params.name + " : " + req.params.age);
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    // database work here
+    res.send('successfully posted data');
 });
 
 app.listen(3000);

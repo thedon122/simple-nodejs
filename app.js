08a07d91fa62852c3010303266a376c2b3a83966
thedon122 <donovan122@yahoo.com>
2021-12-04 16:43:00 +0100

    validation update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const Joi = require('joi');
 const bodyParser = require('body-parser');
+const { result } = require('lodash');
 const app = express();
 
 app.use('/public',express.static(path.join(__dirname,'static')));
@@ -13,8 +15,19 @@ app.get('/',(req,res)=>{
 
 app.post('/',(req,res)=>{
     console.log(req.body);
-    // database work here
-    res.json({success : true});
+    const schema = Joi.object().keys({
+        email : Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(10).required()
+    });
+     const validation = schema.validate(req.body,(err,result)=>{
+        if(err){
+            console.log(err);
+            res.send('an error has occurred');
+        }
+        console.log(result);    
+        res.send('successfully posted data');   
+    });
+    res.send(validation);
 });
 
 app.listen(3000);

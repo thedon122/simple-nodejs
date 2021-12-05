487c7a1f47cd6fb192bf12cae79b3aac0b0f7ee1
thedon122 <donovan122@yahoo.com>
2021-12-05 18:22:00 +0100

    middleware

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/public',express.static(path.join(__dirname,'static')));
-app.set('view engine','ejs');
-app.get('/:userQuery',(req,res)=>{
-    res.render('index',{data :{userQuery: req.params.userQuery,
-                                searchResults : ['book1','book2','book3'],
-                                loggedIn : true,
-                                username : 'lkjfuydc'}});
+app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    req.banana = 'banana';
+    next();
+});
+
+app.get('/',(req,res)=>{
+    console.log(req.banana);
+    res.send('Middleware');
 });
 
 app.listen(3000);

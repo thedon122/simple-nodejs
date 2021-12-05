78166eba67181dbdbf08c37010c57d965d8bc357
thedon122 <donovan122@yahoo.com>
2021-12-05 17:12:00 +0100

    ejs

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('/public',express.static((path.join(__dirname,'static'))));
-app.set('view engine',ejs);
-app.get('/',(req,res)=>{
-    res.rander('index');
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.set('view engine','ejs');
+app.get('/:userQuery',(req,res)=>{
+    res.render('index',{data :{userQuery: req.params.userQuery,
+                                searchResults : ['book1','book2','book3'],
+                                loggedIn : true,
+                                username : 'lkjfuydc'}});
 });
+
+app.listen(3000);

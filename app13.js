489ca872318004ef1f95bf64d07274c417c7449d
thedon122 <donovan122@yahoo.com>
2021-12-06 15:22:00 +0100

    routes update

--- app13.js
+++ app13.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
+const route = express.Router();
 const app = express();
 
+
+route.use((req,res,next)=>{
+    console.log('middleware being used');
+    next();
+});
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.set('view engine','ejs');
-app.get('/:userQuery',(req,res)=>{
-    res.render('index',{data :{userQuery: req.params.userQuery,
-                                searchResults : ['book1','book2','book3'],
-                                loggedIn : true,
-                                username : 'lkjfuydc'}});
-});
+
+const people = require('./routes/people');
+
+app.use('/people',people);
 
 app.listen(3000);

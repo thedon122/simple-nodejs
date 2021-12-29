c855dcb2fc2d80c27b8e71ac2d548961742afb07
thedon122 <donovan122@yahoo.com>
2021-12-28 19:08:46 -0500

    express update

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const _ = require('lodash');
-let example = _.fill([1,2,3,4,5],"banana",1,4);
-console.log(example);
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
+app.listen(3000);
